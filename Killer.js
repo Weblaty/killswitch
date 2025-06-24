8e7e1601c1a34c80c4c599cb8709faa68d8c75bd
Weblaty <almasaraweades@gmail.com>
2025-06-24 08:16:18 +0300

    Update Killer.js

--- Killer.js
+++ Killer.js
@@ -1,1 +1,5 @@
-document.body.innerHTML = "<h1 style='color:red; text-align:center;'>SITE DISABLED - CONTACT SUPPORT</h1>";
+// Killer.js by Weblaty
+
+window.onload = function() {
+    document.body.innerHTML = "<h1 style='color:red; text-align:center;'>SITE DISABLED - CONTACT SUPPORT</h1>";
+};
